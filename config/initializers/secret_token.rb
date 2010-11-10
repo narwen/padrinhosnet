37d490231462a9c3e34b9f95457ce8cdda801796
Gabriel Reis and Thais Camilo <narwen+greis+narwen@gmail.com>
2010-11-09 21:11:27 -0500

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Padrinhosnet::Application.config.secret_token = 'e136794462687dd80e92a762bbf41244f21f5cbf7f1fadedd98eecb23a41d69e9c8e0d79c639096316380671da994b0c1b4c62cc5c80dfe851617902b9a29491'
